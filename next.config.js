3f3e030682cc0a54b23b3c37304a52538076367d
damborskymatias-lang <damborskymatias@gmail.com>
2026-09-09 22:22:26 +0200

    Refactor next.config.js for strict mode and plugins

--- next.config.js
+++ next.config.js
@@ -1,22 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  webpack: (config, { webpack }) => {
-    config.plugins.push(
-      new webpack.IgnorePlugin({
-        resourceRegExp: /^@lov\/ 
-      }),
-      new webpack.IgnorePlugin({
-        resourceRegExp: /\.(compositions|spec|test)\.(tsx|ts|js|jsx)$/,
-      })
-    );
+  reactStrictMode: true,
+  webpack: (config, { isServer }) => {
     return config;
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
